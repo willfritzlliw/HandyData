59be5e343f7403b0f3c7fe2441f720252b2c5cd9
William Fritz <willfritzlliw@gmail.com>
2022-11-09 16:07:00 -0500

    Added lists of words
    great for random words

---
+++ words.py
@@ -0,0 +1,39 @@
+
+verbs = ['be', 'have', 'do', 'say', 'go', 'can', 'get', 'would', 'make', 'know', 'will', 'think', 'take', 'see', 'come', 'could', 'want', 'look', 'use', 'find', 'give', 'tell', 'work', 'may', 'should', 'call', 'try', 'ask', 'need',
+        'feel', 'become', 'leave', 'put', 'mean', 'keep', 'let', 'begin', 'seem', 'help', 'talk', 'turn', 'start', 'might', 'show', 'hear', 'play', 'run', 'move', 'like', 'live', 'believe', 'hold', 'bring', 'happen', 'must', 
+        'write', 'provide', 'sit', 'stand', 'lose', 'pay', 'meet', 'include', 'continue', 'set', 'learn', 'change', 'lead', 'understand', 'watch', 'follow', 'stop', 'create', 'speak', 'read', 'allow', 'add', 'spend', 'grow', 
+        'open', 'walk', 'win', 'offer', 'remember', 'love', 'consider', 'appear', 'buy', 'wait', 'serve', 'die', 'send', 'expect', 'build', 'stay', 'fall', 'cut', 'reach', 'kill', 'remain']
+
+adjectives = ['other', 'new', 'good', 'high', 'old', 'great', 'big', 'American', 'small', 'large', 'national', 'young', 'different', 'black', 'long', 'little', 'important', 'political', 'bad', 'white', 'real', 'best', 'right', 
+            'social', 'only', 'public', 'sure', 'low', 'early', 'able', 'human', 'local', 'late', 'hard', 'major', 'better', 'economic', 'strong', 'possible', 'whole', 'free', 'military', 'true', 'federal', 'international', 
+            'full', 'special', 'easy', 'clear', 'recent', 'certain', 'personal', 'open', 'red', 'difficult', 'available', 'likely', 'short', 'single', 'medical', 'current', 'wrong', 'private', 'past', 'foreign', 'fine', 
+            'common', 'poor', 'natural', 'significant', 'similar', 'hot', 'dead', 'central', 'happy', 'serious', 'ready', 'simple', 'left', 'physical', 'general', 'environmental', 'financial', 'blue', 'democratic', 'dark', 
+            'various', 'entire', 'close', 'legal', 'religious', 'cold', 'final', 'main', 'green', 'nice', 'huge', 'popular', 'traditional', 'cultural']
+
+nouns = ['time', 'year', 'people', 'way', 'day', 'man', 'thing', 'woman', 'life', 'child', 'world', 'school', 'state', 'family', 'student', 'group', 'country', 'problem', 'hand', 'part', 'place', 'case', 'week', 'company', 
+        'system', 'program', 'question', 'work', 'government', 'number', 'night', 'point', 'home', 'water', 'room', 'mother', 'area', 'money', 'story', 'fact', 'month', 'lot', 'right', 'study', 'book', 'eye', 'job', 'word', 
+        'business', 'issue', 'side', 'kind', 'head', 'house', 'service', 'friend', 'father', 'power', 'hour', 'game', 'line', 'end', 'member', 'law', 'car', 'city', 'community', 'name', '', 'president', 'team', 'minute', 'idea',
+        'kid', 'body', 'information', 'back', 'parent', 'face', 'others', 'level', 'office', 'door', 'health', 'person', 'art', 'war', 'history', 'party', 'result', 'change', 'morning', 'reason', 'research', 'girl', 'guy', 
+        'moment', 'air', 'teacher', 'force', 'education']
+
+pronouns = ['it', 'I', 'you', 'he', 'they', 'we', 'she', 'who', 'them', 'me', 'him', 'one', 'her', 'us', 'something', 'nothing', 'anything', 'himself', 'everything', 'someone', 'themselves', 'everyone', 'itself', 'anyone', 'myself']
+
+colors = ['red', 'blue', 'green', 'yellow', 'purple', 'pink', 'orange', 'brown', 'black', 'white', 'gray', 'gold', 'silver', 'navyblue', 'skyblue', 'limegreen', 'teal', 'indigo', 'magenta', 'violet', 'khaki', 'salmon', 
+        'crimson', 'lavender', 'plum', 'blueviolet', 'olive', 'cyan', 'maroon', 'beige']
+
+places = ['Airport', 'AmusementPark', 'Bakery', 'Bank', 'Bicyclepath', 'Bookstore', 'Bridge', 'Busstop', 'Cafe', 'Church', 'Cinema', 'Citycenter', 'Clothingstore', 'Court', 'Crossroads', 'Factory', 'Farm', 'Flyover', 'Garage', 
+        'Gym', 'Hospital', 'Hotel', 'Library', 'Market', 'Museum', 'Park', 'Parkinglot', 'Pedestriancrossing', 'Petrolstation', 'Pharmacy', 'Playground', 'Policestation', 'Postoffice', 'Restaurant', 'Roadsign', 'Roundabout', 
+        'School', 'Shoeshop', 'Sidewalk', 'Sportscenter', 'Streetlights', 'Supermarket', 'Swimmingpool', 'Telephonebooth', 'Theater', 'Trafficjam', 'Trafficlights', 'Trainstation', 'Underpass', 'University', 'Zoo']
+
+jobs = ['InformationSecurityAnalyst', 'NursePractitioner', 'PhysicianAssistant', 'MedicalandHealthServicesManager', 'SoftwareDeveloper', 'DataScientist', 'FinancialManager', 'Cashier', 'Foodpreparationworker', 'Stockingassociate',
+        'Laborer', 'Janitor', 'Constructionworker', 'Bookkeeper', 'Server', 'Novelist', 'Racecardrivinginstructor', 'Foodcritic', 'Eventplanner', 'Sommelier', 'Racecarmechanic', 'Videogamedesigner', 'Voice-overartist', 'Teachingassistant',
+        'Earlychildhoodeducationteacher', 'Soundengineeringtechnician', 'Eventplanner', 'Heavyequipmentoperator', 'Contractor', 'Recruiter', 'Clergy', 'Operatingengineers', 'Teachers', 'Specialeducationteachers', 'Educationadministrators', 
+        'Officesupervisors', 'Physicaltherapists', 'Securityandfinancialservicessalespersons', 'DesignIntern', 'Scientist', 'Technologist', 'CurriculumDeveloper', 'ImplementationSpecialist', 'RockstarTeacher', 'FullStackSoftwareEngineer', 'ChiefRevenueOfficer']
+
+animals = ['Aardvark', 'Alligator', 'Alpaca', 'Anaconda', 'Ant', 'Anteater', 'Antelope', 'Aphid', 'Armadillo', 'Asp', 'Ass', 'Baboon', 'Badger', 'BaldEagle', 'Barracuda', 'Bass', 'BassetHound', 'Bat', 'BeardedDragon', 
+        'Beaver', 'Bedbug', 'Bee', 'Bee-eater', 'Bird', 'Bison', 'Blackpanther', 'BlackWidowSpider', 'BlueJay', 'BlueWhale', 'Bobcat', 'Buffalo', 'Butterfly', 'Buzzard', 'Camel', 'CanadaLynx', 'Carp', 'Cat', 'Caterpillar', 
+        'Catfish', 'Cheetah', 'Chicken', 'Chimpanzee', 'Chipmunk', 'Cobra', 'Cod', 'Condor', 'Cougar', 'Cow', 'Coyote', 'Crab', 'CraneFly', 'Cricket', 'Crocodile', 'Crow', 'Cuckoo', 'Deer', 'Dinosaur', 'Dog', 'Dolphin', 
+        'Donkey', 'Dove', 'Dragonfly', 'Duck', 'Eagle', 'Eel', 'Elephant', 'Emu', 'Falcon', 'Ferret', 'Finch', 'Fish', 'Flamingo', 'Flea', 'Fly', 'Fox', 'Frog', 'Goat', 'Goose', 'Gopher', 'Gorilla', 'GuineaPig', 'Hamster', 
+        'Hare', 'Hawk', 'Hippopotamus', 'Horse', 'Hummingbird', 'HumpbackWhale', 'Husky', 'Iguana', 'Impala', 'Kangaroo', 'Lemur', 'Leopard', 'Lion', 'Lizard', 'Llama', 'Lobster', 'Margay', 'Monitorlizard', 'Monkey', 'Moose', 
+        'Mosquito', 'Moth', 'MountainZebra', 'Mouse', 'Mule', 'Sloth', 'Snail', 'Snake', 'Spider', 'Tiger', 'Whale', 'Wolf', 'Wombat', 'Zebra', 'Octopus', 'Orca', 'Ostrich', 'Otter', 'Owl', 'Ox', 'Oyster', 'Panda', 
+        'Parrot', 'Peacock', 'Pelican', 'Penguin', 'Perch', 'Pheasant', 'Pig', 'Pigeon', 'Polarbear', 'Porcupine', 'Quagga', 'Rabbit', 'Raccoon', 'Rat', 'Rattlesnake', 'RedWolf', 'Rooster', 'Seal', 'Sheep', 'Skunk']
